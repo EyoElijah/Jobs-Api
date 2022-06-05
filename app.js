a52ad6389e6f1ec83ef903acdccce7517b09576f
EyoElijah <eyoelijah@gmail.com>
2022-06-05 07:20:49 +0100

    swagger docummentation

--- app.js
+++ app.js
@@ -33,9 +33,18 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+//swager
+
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 app.get("/", (req, res) => {
-  res.send("jobs api");
+  res.send("<h1>Jobs API</h1><a href='/api-docs/'>Documentation</a>");
 });
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 // routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authentcateUser, jobsRouter);
